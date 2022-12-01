d0ef30fc07b9a2631b09c22ffd396980593b57fe
shannone <shannone426@gmail.com>
2022-12-01 12:47:31 -0500

    cart slice add remove items , price, quantity added

--- 01-starting-project/src/store/cart-slice.js
+++ 01-starting-project/src/store/cart-slice.js
@@ -1,22 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 createSlice({
-    name: 'cart',
-    initialState: {
-        items: [],
-        totalQuantity: 0,
-        //totalAmount: 0,
-
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    //totalAmount: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const exsistingItem = state.items.find((item) => item.id === newItem.id);
+      if (!exsistingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          itemName: newItem.title,
+        });
+      } else {
+        exsistingItem.quantity++;
+        exsistingItem.totalPrice = exsistingItem.itemtotalPrice + newItem.price;
+      }
     },
-        reducers: {
-            addItemToCart(state, action) {
-const newItem = action.payload;
-const exsistingItem = state.items.find(item => item.id === newItem.id);
-if (!exsistingItem) {
-    state.items.push({item.id: newItem.id, price: newItem.price, quantity: 1, totalPrice:  newItem.price});
-};
-            },
-            removeItemFromCart(state, action) {
-        }  
-    } 
+    removeItemFromCart(state, action) {},
+  },
 });
