486ec01ed253ee9bcf560b5be8c910227942fbd1
shannone <shannone426@gmail.com>
2022-12-01 11:00:27 -0500

    restarting yet again

--- 01-starting-project/src/components/Cart/CartButton.js
+++ 01-starting-project/src/components/Cart/CartButton.js
@@ -1,22 +1,13 @@
 import classes from "./CartButton.module.css";
-import { uiActions } from "../../store/ui-slice";
-import { useDispatch } from "react-redux";
+
+
 
 const CartButton = (props) => {
-  const dispatch = useDispatch();
-
-  // const handleClick = () => {
-  //   dispatch(uiActions.setUiState({
-  //     cartButton:!props.cartButton,
-  //   }));
-  // };
-
-  const toggleCartHandler = () => {
-    dispatch(uiActions.toggle());
-  };
+ 
+  
 
   return (
-    <button className={classes.button} onClick={toggleCartHandler}>
+    <button className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
